1fc9e51ceac962f1e1dfcd08db0fe0f55689ed7f
Lyann <bourlon.lyann84@gmail.com>
2020-02-21 14:32:05 +0100

    Updates

---
+++ precache-manifest.5c69330d38424618211c07686f1e6f22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda4142ac27ced01217b6b36fed1ec81",
+    "url": "/coco_broderies/index.html"
+  },
+  {
+    "revision": "84f1f51f15f6ba171af6",
+    "url": "/coco_broderies/static/css/main.b16b0178.chunk.css"
+  },
+  {
+    "revision": "c16240833edf81b8f378",
+    "url": "/coco_broderies/static/js/2.146db7c7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/coco_broderies/static/js/2.146db7c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f1f51f15f6ba171af6",
+    "url": "/coco_broderies/static/js/main.b9c0dfbd.chunk.js"
+  },
+  {
+    "revision": "04ac4039634eaf82d078",
+    "url": "/coco_broderies/static/js/runtime-main.e7ddcdf0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coco_broderies/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.a5de96e50b50977eef413865c3dad51c.js"
+  "/coco_broderies/precache-manifest.5c69330d38424618211c07686f1e6f22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coco_broderies/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
